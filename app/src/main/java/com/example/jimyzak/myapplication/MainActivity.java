5748df51bb35fcc53ca65560ef172cebf504213b
zakarya jimy <zakarya.jimy@gmail.com>
2015-10-27 10:46:34 +0100

    feat (MyApp) refactor view-ZJ

--- app/src/main/java/com/example/jimyzak/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jimyzak/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +20,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button buttonAuth=(Button) findViewById(R.id.buttonAuth);
+        buttonAuth.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+        Button buttonEnr=(Button) findViewById(R.id.buttonEnr);
+        buttonEnr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
     }
@@ -51,16 +58,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.buttonAuth:
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-                break;
-
-            case R.id.buttonEnr:
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                break;
-        }
-
-    }
 }
