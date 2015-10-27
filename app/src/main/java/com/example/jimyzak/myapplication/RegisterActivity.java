c6c94b092330fff44a9a5d2987517a86004af635
marwensaid <marwensaidi1@gmail.com>
2015-10-27 19:17:46 +0100

    feat(Android) refactor  -- MS

--- app/src/main/java/com/example/jimyzak/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/jimyzak/myapplication/RegisterActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
