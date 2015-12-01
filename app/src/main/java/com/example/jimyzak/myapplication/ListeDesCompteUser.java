5229245c6eb5e92801b3fc508ca378f0a3cbc3e6
marwensaid <marwensaidi1@gmail.com>
2015-12-01 20:10:59 +0100

    refactor(Android) code pretty -- MS

--- app/src/main/java/com/example/jimyzak/myapplication/ListeDesCompteUser.java
+++ app/src/main/java/com/example/jimyzak/myapplication/ListeDesCompteUser.java
@@ -29,25 +29,29 @@ public class ListeDesCompteUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_accounts);
         AQuery aq = new AQuery(this);
-        final ArrayList<Personne> listCompteUser = new ArrayList<Personne>();
+        final ArrayList<Personne> listCompteUser = new ArrayList<>();
 
         aq.ajax(url, JSONArray.class, new AjaxCallback<JSONArray>() {
             @Override
             public void callback(String url, JSONArray json, AjaxStatus status) {
                 ListView lst = (ListView) findViewById(R.id.list_accounts);
                 if (json != null) {
-                    //successful ajax call, show status code and json content
 
                     for (int i = 0; i < json.length(); i++) {
                         try {
                             JSONObject o = json.getJSONObject(i);
                             Personne a = new Personne();
-                            if (!o.getString("nom").isEmpty() && !o.getString("prenom").isEmpty() && !o.getString("connectee").isEmpty() && !o.getString("timeCreation").isEmpty()) {
+
+                            if (!o.getString("nom").isEmpty()
+                                    && !o.getString("prenom").isEmpty()
+                                    && !o.getString("connectee").isEmpty()
+                                    && !o.getString("timeCreation").isEmpty()) {
                                 a.setNom(o.getString("nom"));
                                 a.setPrenom(o.getString("prenom"));
                                 a.setConnectee(o.getString("connectee"));
                                 a.setTimeCreation(o.getString("timeCreation"));
                                 a.setId(o.getString("id"));
+
                             } else {
                                 throw new JSONException("stack //TODO");
                             }
