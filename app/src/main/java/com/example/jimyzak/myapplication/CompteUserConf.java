df0505ea8755c845a88be4b8f855514d8ac95bc5
marwensaid <marwensaidi1@gmail.com>
2015-12-01 19:41:18 +0100

    feat(Android) add seq of method {commit for repar}-- MS

--- app/src/main/java/com/example/jimyzak/myapplication/CompteUserConf.java
+++ app/src/main/java/com/example/jimyzak/myapplication/CompteUserConf.java
@@ -1,12 +1,46 @@
 package com.example.jimyzak.myapplication;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by marwen on 01/12/15.
  */
-public class CompteUserConf {
-    public CompteUserConf(ListeDesCompteUser listeDesCompteUser, ArrayList<Personne> listCompteUser) {
+public class CompteUserConf extends BaseAdapter {
 
+    private ListeDesCompteUser listeDesCompteUser;
+    public List<Personne> listCompteUser;
+    public int positionA;
+
+    public CompteUserConf(ListeDesCompteUser listeDesCompteUser, ArrayList<Personne> listCompteUser) {
+        this.listeDesCompteUser = listeDesCompteUser;
+        this.listCompteUser = listCompteUser;
     }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+
 }
