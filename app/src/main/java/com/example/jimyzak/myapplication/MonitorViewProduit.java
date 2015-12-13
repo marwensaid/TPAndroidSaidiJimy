17079a74bd870c30b728e9f720980b2b8c579578
marwensaid <marwensaidi1@gmail.com>
2015-12-13 13:34:50 +0100

    fix(MonitorViewProduit)  fix for ProduitItems--MS

---
+++ app/src/main/java/com/example/jimyzak/myapplication/MonitorViewProduit.java
@@ -0,0 +1,10 @@
+package com.example.jimyzak.myapplication;
+
+import android.widget.TextView;
+
+/**
+ * Created by marwen on 13/12/15.
+ */
+public class MonitorViewProduit {
+    public TextView nom;
+}
