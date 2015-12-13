c2c3736448b486b352d88c250272748dfac3a525
marwensaid <marwensaidi1@gmail.com>
2015-12-13 08:54:35 +0100

    feat(Android-app) prepare listProduit and corresponding activity without impl--MS

---
+++ app/src/main/java/com/example/jimyzak/myapplication/ListProduit.java
@@ -0,0 +1,15 @@
+package com.example.jimyzak.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by marwen on 13/12/15.
+ */
+public class ListProduit extends AppCompatActivity {
+
+}
+class ListProduits extends AppCompatActivity {
+
+}
+
+
