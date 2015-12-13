2cc5b0a9b5d86580a6c6b295b499028b55b4676b
marwensaid <marwensaidi1@gmail.com>
2015-12-13 08:25:12 +0100

    feat(Android-app) create monitorView for ViewItems--MS

---
+++ app/src/main/java/com/example/jimyzak/myapplication/MonitorViewMenu.java
@@ -0,0 +1,10 @@
+package com.example.jimyzak.myapplication;
+
+import android.widget.TextView;
+
+/**
+ * Created by marwen on 13/12/15.
+ */
+public class MonitorViewMenu {
+    public TextView libelleMenu;
+}
